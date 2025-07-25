b483ff00ae599999003ab1cf1cdf99d1c9430c7c
Niya <niyaabraham10@gmail.com>
2025-07-25 18:32:14 +0530

    feat: Add initial responsive profile page frontend

--- routes/workerRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/workerRoutes.js
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.send('Worker route working!');
-});
-
-module.exports = router;
